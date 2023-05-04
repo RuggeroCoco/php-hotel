208c2ff25858646a14d5428f29c289b579bdb87e
RuggeroCoco <ruggerococo94@gmail.com>
2023-05-04 19:39:39 +0200

    Print the data

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+     <?php foreach($hotels as $key => $hotel) {?>
+        <div>
+            <h3> <?php echo $hotel['name'];?> </h3>
+            <p> <?php echo $hotel['description']?> </p>
+            <?php if ($hotel['parking']) { ?>
+                <p>L' <?php echo $hotel['name'];?> è provvisto di parcheggio</p>
+            <?php } else { ?>
+                <p>L' <?php echo $hotel['name'];?> non è provvisto di parcheggio</p>
+            <?php } ?>
+            <p>Voto medio: <?php echo $hotel['vote'];?></p>
+            <p>Distanza dal centro: <?php echo $hotel['distance_to_center'];?> km</p>
+        </div>
+    <?php }?>
+</body>
+</html>
